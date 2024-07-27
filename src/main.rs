702a0762cfcb7c898623cc375a029b8a691dec2a
Rafael Morales <rafaelmoralesvenegas@gmail.com>
2024-07-26 21:43:08 -0400

    Spawning a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bevy::{
+    prelude::*,
+    window::{WindowLevel, WindowResolution},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let window = Window {
+        transparent: true,
+        decorations: false,
+        window_level: WindowLevel::AlwaysOnTop,
+        resolution: WindowResolution::new(400.0, 250.0),
+        resizable: false,
+        ..default()
+    };
+
+    App::new()
+        .insert_resource(ClearColor(Color::NONE))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(window),
+            ..default()
+        }))
+        .run();
 }
